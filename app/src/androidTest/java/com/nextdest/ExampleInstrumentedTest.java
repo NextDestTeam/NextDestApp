db55a0e0c9ea2c1be9e2e18f81f7e8f568c58328
Lucas Alessandro <lucas.alessandro2@gmail.com>
2018-11-03 14:18:23 +0100

    Fixing the project.

--- app/src/androidTest/java/com/nextdest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nextdest/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.nextdestapp;
+package com.nextdest;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.nextdestapp", appContext.getPackageName());
+        assertEquals("com.nextdest", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/nextdest/nextdest/MainActivity.java
+++ app/src/main/java/com/nextdest/nextdest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nextdestapp;
+package com.nextdest.nextdest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
