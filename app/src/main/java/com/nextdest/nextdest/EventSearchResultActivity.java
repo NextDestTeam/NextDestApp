4688383b58b66aa28e0f951a0b722ab01caf60d6
Emilia Naambo Shikeenga <emilianaambo@aules-112-194.udl.net>
2018-11-12 20:00:59 +0100

    merging

--- app/src/main/java/com/nextdest/nextdest/EventSearchResultActivity.java
+++ app/src/main/java/com/nextdest/nextdest/EventSearchResultActivity.java
@@ -3,9 +3,12 @@ package com.nextdest.nextdest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class EventSearchResultActivity extends AppCompatActivity {
+
+            ListView imgListView = (ListView) findViewById(R.id.imglistview);
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,9 @@ public class EventSearchResultActivity extends AppCompatActivity {
             spinner.setAdapter(adapter);
             spinnerCost.setAdapter(adapterCost);
             spinnerAct.setAdapter(adapterAct);
+
+
+
         }
 
 
