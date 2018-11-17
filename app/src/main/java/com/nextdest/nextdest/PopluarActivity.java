93404786829b55ee156cb0d8c1597ec1463228f7
Diaa777 <diaa77.eng@gmail.com>
2018-11-17 19:35:13 +0100

    Popular screen has been fixed

--- app/src/main/java/com/nextdest/nextdest/PopluarActivity.java
+++ app/src/main/java/com/nextdest/nextdest/PopluarActivity.java
@@ -3,9 +3,12 @@ package com.nextdest.nextdest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -14,31 +17,47 @@ import com.nextdest.adapter.GridAdabter;
 
 public class PopluarActivity extends AppCompatActivity {
 
-    GridView gridView;
 
-    int images []= {R.drawable.barca,R.drawable.festival,R.drawable.mou,R.drawable.food,R.drawable.dance,R.drawable.party };
+    int images []= {R.drawable.barca,R.drawable.festival,R.drawable.mou,R.drawable.food,R.drawable.tennis,R.drawable.party, R.drawable.tango ,R.drawable.marathon ,R.drawable.cars ,R.drawable.flam ,R.drawable.theater , R.drawable.galler};
     String values [] = {
             "El Classico",
             "Jazz Party",
             "Natural Trip",
             "Diner ",
             "Tines Match",
-            "Classic Party"};
+            "Classic concert",
+            "Tango",
+            "marathon",
+            "Cars race",
+            "Flamingo",
+            "Theater play",
+            "Art gallery"
+            };
 
     Button btSearch;
-    ImageView btnimage;
+    GridView gridView;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popular_event);
 
-        btSearch = (Button) findViewById(R.id.btPopularActivitySearch);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerPeople);
+
+        gridView= (GridView) findViewById(R.id.gridView);
+
+
+
+
+
+       /* Spinner spinner = (Spinner) findViewById(R.id.spinnerPeople);
         Spinner spinnerCost = (Spinner) findViewById(R.id.spinnerCost);
         Spinner spinnerAct = (Spinner) findViewById(R.id.spinnerActivity);
-        // Defining ArrayAdapter
+         Defining ArrayAdapter
         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.people_array, android.R.layout.simple_spinner_item);
 
@@ -54,24 +73,47 @@ public class PopluarActivity extends AppCompatActivity {
         spinnerCost.setAdapter(adapterCost);
         spinnerAct.setAdapter(adapterAct);
 
-        btSearch.setOnClickListener(new View.OnClickListener() {
+
+         */
+
+
+
+       /* btSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent i = new Intent(v.getContext(),EventSearchResultActivity.class);
                 startActivity(i);
             }
-        });
+        });*/
 
 
 
 
-        gridView = (GridView)findViewById(R.id.gridView);
+
 
 
         GridAdabter gridAdabter = new GridAdabter (this , images ,values);
         gridView.setAdapter(gridAdabter);
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(),SelectedEventActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+
+
+
+
 
 
 
