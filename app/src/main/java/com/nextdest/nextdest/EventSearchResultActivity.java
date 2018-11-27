6d030dda2fcca0746e3f1f9b9327cf2e037f4fe7
emilianaambo <emiliashikeenga@gmail.com>
2018-11-27 11:02:57 +0100

    issues #68

--- app/src/main/java/com/nextdest/nextdest/EventSearchResultActivity.java
+++ app/src/main/java/com/nextdest/nextdest/EventSearchResultActivity.java
@@ -1,53 +1,38 @@
 package com.nextdest.nextdest;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Spinner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class EventSearchResultActivity extends AppCompatActivity {
 
-    ListView imgListView;
-    Button btnSearch;
-    String[] eventNames;
-    int[] eventImages = {R.drawable.barca, R.drawable.festival, R.drawable.food, R.drawable.music2};
-    List rowItems;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_search_results);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_results);
+            Spinner spinner = (Spinner) findViewById(R.id.spinnerPeople);
+            Spinner spinnerCost = (Spinner) findViewById(R.id.spinnerCost);
+            Spinner spinnerAct = (Spinner) findViewById(R.id.spinnerActivity);
+            // Defining ArrayAdapter
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.people_array, android.R.layout.simple_spinner_item);
 
+            ArrayAdapter<CharSequence> adapterCost = ArrayAdapter.createFromResource(this,
+                    R.array.cost_array, android.R.layout.simple_spinner_item);
 
-        rowItems = new ArrayList();
-        //eventImages = getResources().getIntArray(R.array.event_imagesArr);
-        eventNames = getResources().getStringArray(R.array.event_namesArr);
+            ArrayAdapter<CharSequence> adapterAct = ArrayAdapter.createFromResource(this,
+                    R.array.activity_array, android.R.layout.simple_spinner_item);
+// Specify the layout
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Applying adapter to spinner
+            spinner.setAdapter(adapter);
+            spinnerCost.setAdapter(adapterCost);
+            spinnerAct.setAdapter(adapterAct);
+        }
 
-
-        SearchAdapter searchAdapter = new SearchAdapter(EventSearchResultActivity.this, eventNames, eventImages);
-        imgListView = (ListView) findViewById(R.id.imglistview);
-        imgListView.setAdapter(searchAdapter);
-        imgListView.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent mIntent = new Intent(EventSearchResultActivity.this,SelectedEventActivity.class);
-                startActivity(mIntent);
-            }
-        });
-
-    }
 
 
 
 }
-
