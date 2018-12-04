a487624f0f0e89afa24e12462d81bf4442348779
Diaa777 <diaa77.eng@gmail.com>
2018-12-04 20:08:53 +0100

    Issue_#26 : search activity and get location for each activity

--- app/src/main/java/com/nextdest/nextdest/Filters.java
+++ app/src/main/java/com/nextdest/nextdest/Filters.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 
 public class Filters extends Fragment {
@@ -13,23 +14,30 @@ public class Filters extends Fragment {
 
 
 
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filters, container, false);
+        View view = inflater.inflate(R.layout.fragment_filters, container, false);
+
+
+        Spinner spinner1 = view.findViewById(R.id.spinnerPeople);
+        Spinner spinner2 = view.findViewById(R.id.spinnerCost);
+        Spinner spinner3 = view.findViewById(R.id.spinnerActivity);
+
+
+       // ArrayAdapter<CharSequence>
 
 
 
-
-
+        return view;
 
     }
+
 
 
 
